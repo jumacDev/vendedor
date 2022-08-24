8ed8a1e34ed2a3c8d2e28a0822042032162f1a26
Millo520 <millochance@gmail.com>
2022-08-24 15:14:07 -0500

    Firebase implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:vendedor/UI/Pages/login_page.dart';
-import 'package:vendedor/firebase_options.dart';
 
-void main() async {
+void main()  {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  Firebase.initializeApp();
   runApp(const SellerApp());
 }
 
 class SellerApp extends StatefulWidget {
   const SellerApp({Key? key}) : super(key: key);
-
   @override
   State<SellerApp> createState() => _SellerAppState();
 }
 
 class _SellerAppState extends State<SellerApp> {
   @override
+  void initState(){
+    super.initState();
+    getUsers();
+  }
+  void getUsers() async {
+    CollectionReference collectionReference = FirebaseFirestore.instance.collection("chile");
+    QuerySnapshot users = await collectionReference.get();
+
+    if (users.docs.length != 0){
+      for (var doc in users.docs){
+        print(doc.data());
+      }
+    }
+}
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
